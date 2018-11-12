c9d1a3889204341592d4420c87286f2cb2757b44
unknown <chinkayo18@gmail.com>
2018-11-12 18:57:41 +0900

    fix login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,16 +5,16 @@
 @section('content')
   <div class="wrapper">
     <form class="form-signin" action="{{route('post_login')}}" method="POST">
-      {{ csrf_field() }}       
-      <h2 class="form-signin-heading">Please login</h2>
+      {{ csrf_field() }}
+      <h2 class="form-signin-heading">ログインしてください。</h2>
       <input type="text" class="form-control" name="email" placeholder="Email Address" value="{{old('email')}}"/>
       @if ($errors->has('email'))
           <p>{{$errors->first('email')}}</p>
       @endif
-      <input type="password" class="form-control" name="password" placeholder="Password"/>  
+      <input type="password" class="form-control" name="password" placeholder="Password"/>
       @if ($errors->has('password'))
           <p>{{$errors->first('password')}}</p>
-      @endif    
+      @endif
       <label class="checkbox">
         <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
       </label>
