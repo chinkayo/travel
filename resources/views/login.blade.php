97d4fdf1118c1d19c038f95711dfe413d9788e03
Gsong224 <song.g224@gmail.com>
2018-11-12 19:29:09 +0900

    #1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,11 +6,7 @@
   <div class="wrapper">
     <form class="form-signin" action="{{route('post_login')}}" method="POST">
       {{ csrf_field() }}
-<<<<<<< HEAD
       <h2 class="form-signin-heading">このメッセージがいらない。</h2>
-=======
-      <h2 class="form-signin-heading">ログインください。</h2>
->>>>>>> 69fdded3926c41b9ace72aac43bbd9f8f7b4ca56
       <input type="text" class="form-control" name="email" placeholder="Email Address" value="{{old('email')}}"/>
       @if ($errors->has('email'))
           <p>{{$errors->first('email')}}</p>
@@ -20,7 +16,7 @@
           <p>{{$errors->first('password')}}</p>
       @endif
       <label class="checkbox">
-        <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
+        <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe">記録
       </label>
         <input class="btn btn-lg btn-primary btn-block" type="submit" value="Login">
     </form>
